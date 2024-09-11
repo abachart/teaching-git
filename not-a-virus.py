6abb246ba903690e7905b8f9355e731827b466cd
abachart <abachart01@gmail.com>
2024-09-11 15:49:41 -0700

    removed not-a-virus.py

--- not-a-virus.py
+++
@@ -1,4 +0,0 @@
-from random import *
-
-for i in range(100):
-    print(f'{chr(randint(1,1000))}{chr(randint(1,1000))}{chr(randint(1,1000))}{chr(randint(1,1000))}{chr(randint(1,1000))}{chr(randint(1,1000))}{chr(randint(1,1000))}{chr(randint(1,1000))}{chr(randint(1,1000))}{chr(randint(1,1000))}')
